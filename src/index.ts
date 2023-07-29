0e64845ea13c2f56223d8e8d0a5f58839592a66a
0xterran <0xterran@protonmail.com>
2023-07-29 09:02:09 +0700

    clean up

--- src/index.ts
+++ src/index.ts
@@ -11,91 +11,91 @@ import { initFirebase } from "@/services/firebase";
 import config from "@/config.env";
 import { initStripe } from "./services/stripe";
 
-const initCreds = async () => {
+const init = async () => {
   await initFirebase();
   await initStripe();
-};
-initCreds();
 
-const SOCKETS_PORT = process.env.PORT || 8080;
+  const SOCKETS_PORT = process.env.PORT || 8080;
 
-const yogaPlugins = [];
-if (process.env.NODE_ENV !== "development") {
-  yogaPlugins.push(useDisableIntrospection());
-}
+  const yogaPlugins = [];
+  if (process.env.NODE_ENV !== "development") {
+    yogaPlugins.push(useDisableIntrospection());
+  }
 
-const yoga = createYoga({
-  schema,
-  graphiql: {
-    // Use WebSockets in GraphiQL
-    subscriptionsProtocol: "WS",
-  },
-  plugins: yogaPlugins,
-  cors: {
-    origin: config.GRAPHQL.CORS_ORIGINS,
-    methods: ["GET", "POST", "OPTIONS"],
-  },
-});
-
-// Get NodeJS Server from Yoga
-const server = createServer(yoga);
-
-// Create WebSocket server instance from our Node server
-const wsServer = new WebSocketServer({
-  server,
-  path: yoga.graphqlEndpoint,
-});
-
-// Variable to hold the number of active connections
-let activeConnections = 0;
-
-wsServer.on("connection", function connection(ws) {
-  activeConnections++; // increment the active connections
-  console.log("Number of active connections: " + activeConnections);
-
-  ws.on("close", function close() {
-    activeConnections--; // decrement the active connections
-    console.log("Number of active connections: " + activeConnections);
-  });
-});
-
-// Integrate through Yoga's Envelop instance
-useServer(
-  {
-    execute: (args: any) => args.rootValue.execute(args),
-    subscribe: (args: any) => args.rootValue.subscribe(args),
-    onSubscribe: async (ctx, msg) => {
-      console.log(
-        `A new GraphQL operation received on subscription channel...`
-      );
-      const { schema, execute, subscribe, contextFactory, parse, validate } =
-        yoga.getEnveloped({
-          ...ctx,
-          req: ctx.extra.request,
-          socket: ctx.extra.socket,
-          params: msg.payload,
-        });
-
-      const args = {
-        schema,
-        operationName: msg.payload.operationName,
-        document: parse(msg.payload.query),
-        variableValues: msg.payload.variables,
-        contextValue: await contextFactory(),
-        rootValue: {
-          execute,
-          subscribe,
-        },
-      };
-
-      const errors = validate(args.schema, args.document);
-      if (errors.length) return errors;
-      return args;
+  const yoga = createYoga({
+    schema,
+    graphiql: {
+      // Use WebSockets in GraphiQL
+      subscriptionsProtocol: "WS",
     },
-  },
-  wsServer
-);
+    plugins: yogaPlugins,
+    cors: {
+      origin: config.GRAPHQL.CORS_ORIGINS,
+      methods: ["GET", "POST", "OPTIONS"],
+    },
+  });
 
-server.listen(SOCKETS_PORT, () => {
-  console.log(`Listening on PORT ${SOCKETS_PORT}/graphql`);
-});
+  // Get NodeJS Server from Yoga
+  const server = createServer(yoga);
+
+  // Create WebSocket server instance from our Node server
+  const wsServer = new WebSocketServer({
+    server,
+    path: yoga.graphqlEndpoint,
+  });
+
+  // Variable to hold the number of active connections
+  let activeConnections = 0;
+
+  wsServer.on("connection", function connection(ws) {
+    activeConnections++; // increment the active connections
+    console.log("Number of active connections: " + activeConnections);
+
+    ws.on("close", function close() {
+      activeConnections--; // decrement the active connections
+      console.log("Number of active connections: " + activeConnections);
+    });
+  });
+
+  // Integrate through Yoga's Envelop instance
+  useServer(
+    {
+      execute: (args: any) => args.rootValue.execute(args),
+      subscribe: (args: any) => args.rootValue.subscribe(args),
+      onSubscribe: async (ctx, msg) => {
+        console.log(
+          `A new GraphQL operation received on subscription channel...`
+        );
+        const { schema, execute, subscribe, contextFactory, parse, validate } =
+          yoga.getEnveloped({
+            ...ctx,
+            req: ctx.extra.request,
+            socket: ctx.extra.socket,
+            params: msg.payload,
+          });
+
+        const args = {
+          schema,
+          operationName: msg.payload.operationName,
+          document: parse(msg.payload.query),
+          variableValues: msg.payload.variables,
+          contextValue: await contextFactory(),
+          rootValue: {
+            execute,
+            subscribe,
+          },
+        };
+
+        const errors = validate(args.schema, args.document);
+        if (errors.length) return errors;
+        return args;
+      },
+    },
+    wsServer
+  );
+
+  server.listen(SOCKETS_PORT, () => {
+    console.log(`Listening on PORT ${SOCKETS_PORT}/graphql`);
+  });
+};
+init();
